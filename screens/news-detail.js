81363efb059626ff2a1243c2b8e020a01d9e52e6
iqbalmusyaffa <55908032+iqbalmusyaffa@users.noreply.github.com>
2023-10-26 00:42:02 +0700

    update tugas nativebase done

--- screens/news-detail.js
+++ screens/news-detail.js
@@ -1,17 +1,25 @@
-import { Heading, Center, Text } from "native-base";
+import { Heading, Center, Text,Image,Box,ScrollView } from "native-base";
 import { Header } from "../components";
 
 const NewsDetail = ({ route }) => {
   // Get the params
   const params = route.params.item;
   return (
-    <>
+    <ScrollView>
       <Header title={"News"} withBack="true" />
-      <Center flex={1} p={"4"}>
-        <Heading>News Detail</Heading>
-        <Text textAlign={"center"}>{params.title}</Text>
-      </Center>
-    </>
+
+                  <Image
+                    source={{ uri: params.image }}
+                    w="100%"
+                    h="20%"
+                    alt="Data foto"
+                  />
+                  <Box p={4}>
+                  <Text fontSize="sm">{params.date}</Text>
+                <Heading textAlign="left" fontSize="xl" fontWeight="bold">{params.title}</Heading>
+                <Text fontSize="md" fontFamily="Roboto">{params.content}</Text>
+                </Box>
+    </ScrollView>
   );
 };
 
